19a92d802e6484320db292db8d9387e6892a6501
Brayan M <brayanmnz@hotmail.com>
2024-07-31 17:24:41 -0400

    Agregando bootstrap y estilos al formulario de usuarios, agregando card para redirigir de la pagina principal al formulario de usuarios.

--- course-demo/routes/users.js
+++ course-demo/routes/users.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('forms/user-signup', {title: 'Registro de Usuarios', layout: './layouts/forms-layout'});
+router.get('/signup', function (req, res, next) {
+  res.render('forms/user-signup', {
+    title: 'Registro de Usuarios',
+    layout: './layouts/forms-layout'
+  });
 });
-router.post('/process', function(req, res, next) {
+
+router.post('/signup', function (req, res, next) {
   console.log(req.body);
-  res.redirect(303, '/users');
+  res.redirect(303, '/users/signup/thank-you');
+});
+
+router.get('/signup/thank-you', function (req, res, next) {
+  res.render('forms/user-signup-thank-you', {
+    title: 'Gracias',
+    layout: './layouts/forms-layout'
+  });
 });
 
 

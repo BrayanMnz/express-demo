905defc3932089060d8ba58f9a5b43d01e142974
brayan.vargas <brayan.vargas@hicx.com>
2024-07-31 16:10:47 -0400

    Agregando formulario usando bootstrap y un nuevo layout

--- course-demo/routes/users.js
+++ course-demo/routes/users.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('forms/user-signup', {title: 'Registro de Usuarios'});
+  res.render('forms/user-signup', {title: 'Registro de Usuarios', layout: './layouts/forms-layout'});
 });
-
 router.post('/process', function(req, res, next) {
   console.log(req.body);
   res.redirect(303, '/users');
